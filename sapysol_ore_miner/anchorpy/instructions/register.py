863ff899f876efb34b9af173256ae662e8287889
Anton Platonov <anton@platonov.io>
2024-04-08 16:35:39 +0300

    fix register

--- sapysol_ore_miner/anchorpy/instructions/register.py
+++ sapysol_ore_miner/anchorpy/instructions/register.py
@@ -22,10 +22,9 @@ def register(accounts:   RegisterAccounts,
              remaining_accounts: typing.Optional[typing.List[AccountMeta]] = None) -> Instruction:
 
     keys: list[AccountMeta] = [
-        AccountMeta(pubkey=accounts["ore_program"], is_signer=False, is_writable=False),
-        AccountMeta(pubkey=accounts["signer"],      is_signer=True,  is_writable=False),
-        AccountMeta(pubkey=accounts["proof"],       is_signer=False, is_writable=True ),
-        AccountMeta(pubkey=SYS_PROGRAM_ID,          is_signer=False, is_writable=False),
+        AccountMeta(pubkey=accounts["signer"], is_signer=True,  is_writable=False),
+        AccountMeta(pubkey=accounts["proof"],  is_signer=False, is_writable=True ),
+        AccountMeta(pubkey=SYS_PROGRAM_ID,     is_signer=False, is_writable=False),
     ]
     if remaining_accounts is not None:
         keys += remaining_accounts
